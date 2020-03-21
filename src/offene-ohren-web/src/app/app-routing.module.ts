f3bd04ea0cca50d64edf5b2542468dd0ccf77dff
Robin Ehlert <rehlert@outlook.de>
2020-03-21 23:53:03 +0100

    adds startpage

--- src/offene-ohren-web/src/app/app-routing.module.ts
+++ src/offene-ohren-web/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StartPageComponent } from './components/pages/start-page/start-page.component';
+import { HelpSearchComponent } from './components/pages/help-search/help-search.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: StartPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
